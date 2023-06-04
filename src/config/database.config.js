053d76529def4e20d14b7b06b4e08c48d7b8729c
Ryan Putra <ryanputra220101@gmail.com>
2023-06-05 05:37:19 +0800

    Fix database import

--- src/config/database.config.js
+++ src/config/database.config.js
@@ -1,14 +1,17 @@
 import mongoose from "mongoose";
 import "dotenv/config";
-import { MONGO_URI } from process.env;
+const { MONGO_URI } = process.env;
 
-mongoose.connect(MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true})
+await mongoose.connect(MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 const db = mongoose.connection;
 
 db.on("error", console.error.bind(console, "Connection error:"));
 db.once("open", () => {
-    console.log("Database connected!");
+  console.log("Database connected!");
 });
 
-export default db;
+export default mongoose;
--- src/models/Holiday.js
+++ src/models/Holiday.js
@@ -1,4 +1,4 @@
-import db from "../config/database.config";
+import db from "../config/database.config.js";
 
 const HolidaySchema = new db.Schema({
   title: { type: String, required: true },
