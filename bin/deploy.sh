d66da96f70bd89a68a7ebefeea1664a0651c155e
Ryan Putra <ryanputra220101@gmail.com>
2023-06-05 17:29:37 +0800

    Fix docker-compose.yml file name typo

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -15,6 +15,6 @@ scp ./docker-compose.yml "$SERVER_USERNAME@$SERVER_IP":~/docker-compose.yml
 ssh "$SERVER_USERNAME@$SERVER_IP" \
   "mkdir -p ~/talenta-bot/ ;" \
   "mv ~/vm-deployer.sh ~/talenta-bot/vm-deployer.sh ;" \
-  "mv ~/docker-compose.yml ~/talenta-bot/docker-composer.yml ;" \
+  "mv ~/docker-compose.yml ~/talenta-bot/docker-compose.yml ;" \
   "chmod +x ~/talenta-bot/vm-deployer.sh ;" \
   "~/talenta-bot/vm-deployer.sh '$DB_NAME'"
