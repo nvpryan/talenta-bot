60139b8ea7f5527c25cd679eb5390c22caf655f9
Ryan Putra <ryanputra220101@gmail.com>
2023-06-06 02:44:25 +0800

    Fix deployer shell script

--- bin/vm-deployer.sh
+++ bin/vm-deployer.sh
@@ -3,5 +3,5 @@
 set -e
 DB_NAME=$1
 
-docker compose down
-DB_NAME=${DB_NAME} docker compose up -d
+docker compose down -f ./docker-compose.yml
+DB_NAME=${DB_NAME} docker compose up -d -f ./docker-compose.yml
