558e97bd69d0db2214bf149d9a5433bfa73f6128
Ryan Putra <ryanputra220101@gmail.com>
2023-06-05 17:39:10 +0800

    Fix vm-deployer.sh script and change docker tag repo

--- bin/utils.sh
+++ bin/utils.sh
@@ -6,7 +6,7 @@ function get_hash() {
 }
 
 function get_tag() {
-  tag=nvpryan/talentabot:$(date +%y.%m.%d)-$(get_hash)
+  tag=nvpryan/talenta-bot:$(date +%y.%m.%d)-$(get_hash)
   echo "$tag"
 }
 
--- bin/vm-deployer.sh
+++ bin/vm-deployer.sh
@@ -4,4 +4,4 @@ set -e
 DB_NAME=$1
 
 docker compose down
-DB_NAME='$DB_NAME' docker compose up -d
+DB_NAME=$DB_NAME docker compose up -d
